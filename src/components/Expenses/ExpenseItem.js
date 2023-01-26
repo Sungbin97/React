33482bd389742357fcdb9d3e00b702224e49f385
Sungbin97 <csb3694@naver.com>
2023-01-26 22:31:30 +0900

    47 ~ 53

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
-import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import { useState } from "react"; 
 import Card from "../UI/Card";
-function ExpenseItem(props) {
-    
+import "./ExpenseItem.css";
+const ExpenseItem = (props) => {
+  // 컴포넌트 함수에서 직접적으로 호출해야함. 
+  const [title, setTitle] = useState(props.title);
+  
+  const clickHandler = () => {
+    setTitle('Update!');
+  }
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button> {/* jsx문법이 평가될 때 실행이 아닌 클릭될 때 실행이 됨. */}
     </Card>
   );
 }
